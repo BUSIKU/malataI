e56025bc5125f7245a7590e29749eb59d23efb18
BUSIKU <40454829+BUSIKU@users.noreply.github.com>
2024-11-07 18:03:12 +0200

    Update script.js
    adding a new script

--- script.js
+++ script.js
@@ -1,65 +1,105 @@
-const apiKey = '2075a3fef86b405c99465912241310';
-const cityInput = document.getElementById('cityInput');
-const searchButton = document.getElementById('searchButton');
-const weatherInfo = document.querySelector('.weather-info');
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
 
-searchButton.addEventListener('click', () => {
-    const city = cityInput.value;
-    if (city) {
-        fetchWeatherData(city);
-    }
-});
 
-async function fetchWeatherData(city) {
-    try {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2075a3fef86b405c99465912241310&q=Chibombo&aqi=no}`);
-        const data = await response.json();
-        if (data.cod === '404') {
-            alert('City not found');
-        } else {
-            displayWeatherData(data);
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude } = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
+
+    currentWeatherItemsEl.innerHTML = 
+    `<div class="weather-item">
+        <div>Humidity</div>
+        <div>${humidity}%</div>
+    </div>
+    <div class="weather-item">
+        <div>Pressure</div>
+        <div>${pressure}</div>
+    </div>
+    <div class="weather-item">
+        <div>Wind Speed</div>
+        <div>${wind_speed}</div>
+    </div>
+
+    <div class="weather-item">
+        <div>Sunrise</div>
+        <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div>
+    </div>
+    <div class="weather-item">
+        <div>Sunset</div>
+        <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
+    </div>
+    
+    
+    `;
+
+    let otherDayForcast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx == 0){
+            currentTempEl.innerHTML = `
+            <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+            <div class="other">
+                <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+                <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+            </div>
+            
+            `
+        }else{
+            otherDayForcast += `
+            <div class="weather-forecast-item">
+                <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                <div class="temp">Day - ${day.temp.day}&#176;C</div>
+            </div>
+            
+            `
         }
-    } catch (error) {
-        alert('Error fetching the data');
-    }
-}
+    })
 
-function displayWeatherData(data) {
-    const cityName = document.getElementById('cityName');
-    const temp = document.getElementById('temp');
-    const condition = document.getElementById('condition');
-    const humidity = document.getElementById('humidity');
-    const windSpeed = document.getElementById('windSpeed');
-    const localTime = document.getElementById('localTime');
-    const icon = document.getElementById('icon');
-    
-    cityName.textContent = data.name;
-    temp.textContent = Math.round(data.main.temp);
-    condition.textContent = data.weather[0].description;
-    humidity.textContent = data.main.humidity;
-    windSpeed.textContent = data.wind.speed;
-    
-    const weatherIcon = data.weather[0].icon;
-    icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}.png" alt="weather-icon">`;
 
-    // Get the local time of the searched city using the timezone offset
-    const timezoneOffset = data.timezone;
-    const localDate = new Date(new Date().getTime() + (timezoneOffset * 1000));
-    localTime.textContent = `Local Time: ${localDate.toLocaleTimeString()}`;
-    
-    weatherInfo.style.display = 'block';
-}
-
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        fetchWeatherDataByCoords(lat, lon);
-    });
-}
-
-async function fetchWeatherDataByCoords(lat, lon) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=2075a3fef86b405c99465912241310&q=Chibombo&aqi=no`);
-    const data = await response.json();
-    displayWeatherData(data);
+    weatherForecastEl.innerHTML = otherDayForcast;
 }
